e1883506a719af7c007aea5b0f36be7c7a25d71d
Expert 🚀 <fcezarf@gmail.com>
2025-08-03 17:50:20 -0300

    Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,33 @@
-import { styled } from "../styles";
-
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: '4px',
-  border: '0',
-  padding: '4px 8px',
-
-  span : {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
-
+import Image from 'next/image'
+import { HomeContainer, Product } from "../styles/pages/home";
+import camiseta1 from "../assets/1.png";
+import camiseta2 from "../assets/2.png";
+import camiseta3 from "../assets/3.png";
 
 export default function Home() {
   return (
     
-    <Button>
-      <span>Teste</span>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="Camiseta 1" />
 
-      Enviar
-      </Button>
+       <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+       </footer>
+      </Product>
+
+            <Product>
+        <Image src={camiseta2} width={520} height={480} alt="Camiseta 2" />
+
+       <footer>
+        <strong>Camiseta X</strong>
+        <span>R$ 79,90</span>
+       </footer>
+      </Product>
+
+
+
+    </HomeContainer>
   );
 }
